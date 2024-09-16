978c3918af3d519dda64768bee9b6ca49234efe3
nupurpatilappdirect <nupur.patil@appdirect.com>
2024-09-16 18:51:46 +0530

    Initialise
    mon 16 sept

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dataPath = path.join(__dirname, 'data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/data', (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import Grid from './GridLayout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+function App() {
+
+  // console.log(data.layout)
+  return (
+    <div className="App">
+    <Grid></Grid>
+   </div>
+  );
+}
+
+export default App;
